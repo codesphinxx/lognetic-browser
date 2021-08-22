871658d86656217bb66c64fba0e53bc481758460
Code Sphinx <31335736+codesphinxx@users.noreply.github.com>
2021-08-21 23:47:02 -0500

    export additional support classes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 export {default as lognetic} from './log.client';
 
+export {default as WebHelper} from './web.helper';
+
+export {default as BrowserInformer} from './device.informer';
+
 export {default as murmurhash3_32_gc} from './murmurhash3';
